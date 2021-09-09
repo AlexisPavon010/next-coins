9642827ff22a6e6b049c0a1fcc8c5fd6a6052e86
Alexis <themaster034@gmail.com>
2021-09-09 02:45:24 -0300

    test

--- pages/movimientos.js
+++ pages/movimientos.js
@@ -15,29 +15,29 @@ export default function Trade(props) {
             <div className="px-3 md:px-8 h-auto mt-10">
                 <div className="container mx-auto max-w-full">
                     <div className="grid grid-cols-1 px-4 mb-16">
-                        <Movimientos {...props} />
+                        {/* <Movimientos {...props} /> */}
                     </div>
                 </div>
             </div>
         </>
     );
 }
-export async function getServerSideProps(ctx) {
-    const rst = await db.collection(ctx.req.cookies.token).doc('movimientos').collection('order').get()
-    const docs = rst.docs.map((doc) => ({...doc.data()}))
+// export async function getServerSideProps(ctx) {
+//     const rst = await db.collection(ctx.req.cookies.token).doc('movimientos').collection('order').get()
+//     const docs = rst.docs.map((doc) => ({...doc.data()}))
 
 
   
-    if (!docs) {
-      return {
-        props: {}
-      }
-    }
+//     if (!docs) {
+//       return {
+//         props: {}
+//       }
+//     }
   
-    return {
-      props: {
-        tradeFb: JSON.stringify(docs),
-        userUid: ctx.req.cookies.token,
-      }, // will be passed to the page component as props
-    }
-  }
+//     return {
+//       props: {
+//         tradeFb: JSON.stringify(docs),
+//         userUid: ctx.req.cookies.token,
+//       }, // will be passed to the page component as props
+//     }
+//   }
